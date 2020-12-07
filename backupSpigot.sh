35076002a1b76f020cc4b365149d21ab4ff303e7
Jim <jim@Seed.morrison>
2020-12-06 19:46:48 -0500

    backup all the worlds

---
+++ backupSpigot.sh
@@ -0,0 +1,3 @@
+./backup.sh -i /home/minecraft/server/world -o /data/backups/world
+./backup.sh -i /home/minecraft/server/world_nether -o /data/backups/world_nether
+./backup.sh -i /home/minecraft/server/world_the_end -o /data/backups/world_the_end
